31925ca526e5b7a1caab4a5e225105197296719e
nhducminh <nguyenhoangducminh@gmail.com>
2026-03-03 09:14:19 +0700

    Add .gitignore and clean up repository

--- dist/services/RandomSelector.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.selectRandom = void 0;
-function selectRandom(items) {
-    const randomIndex = Math.floor(Math.random() * items.length);
-    return items[randomIndex];
-}
-exports.selectRandom = selectRandom;
